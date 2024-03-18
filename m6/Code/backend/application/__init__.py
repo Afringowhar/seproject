d349331f104ab53f5ca89b3a0921ffaea120c106
Abir Chakraborty <21f2000280@ds.study.iitm.ac.in>
2024-03-18 22:15:34 +0530

    updated the Application ID and API key

--- m6/Code/backend/application/__init__.py
+++ m6/Code/backend/application/__init__.py
@@ -45,5 +45,5 @@ def create_app(conf=LocalDevelopmentConfig):
 
 app,api,celery = create_app()
 
-client = SearchClient.create("RRBO0FF8YF", LocalDevelopmentConfig.SEARCH_API_KEY)
-index = client.init_index('sociogrammers_app')
+client = SearchClient.create("K26CF5W1OF", LocalDevelopmentConfig.SEARCH_API_KEY)
+index = client.init_index('seproject')
